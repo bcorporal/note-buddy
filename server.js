739ae3caf5c361038289b8bf2d0a5e43a2986af4
bcorporal <bcorporal@gmail.com>
2022-04-14 16:06:08 -0400

    Save works, unique IDS created with uuid for notes, deploy to heroku next

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const express = require('express');
 const fs = require("fs");
 const notes = require("./db/db/db.json")
 const path = require('path');
-const uuid = require('./helpers/uuid.js');
+const uuid = require("uuid");
 const app = express();
 
 // Port
 const PORT = process.env.PORT || 3001;
 
-// const notes = require("./Develop/db/db.json");
-//  const { Console } = require('console');
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
@@ -31,21 +29,21 @@ app.get('/notes', (req, res) =>
    
 );
 
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"))
-});
+// app.get("/api/notes", (req, res) => {
+// res.sendFile(path.join(__dirname, "./db/db/db.json"))
+// });
 
 // api route to retrieve notes saved
-app.get('/api/notes', (req, res) =>
-res.sendFile(path.join(__dirname, "/db/db.json"))
-);
+app.get('/api/notes', (req, res) => {
+res.sendFile(path.join(__dirname, "./db/db/db.json"))
+});
 
 app.post('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('/db/db.json'));
+    const notes = JSON.parse(fs.readFileSync('./db/db/db.json'));
     const newNotes = req.body;
     newNotes.id = uuid.v4();
     notes.push(newNotes);
-    fs.writeFileSync("/db/db.json", JSON.stringify(notes))
+    fs.writeFileSync("./db/db/db.json", JSON.stringify(notes))
     res.json(notes);
 });
 
